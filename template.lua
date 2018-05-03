aa4700374102c1062fbc8057cdf5698945a4bcbe
Martin Cuma <m.cuma@utah.edu>
2018-05-03 09:56:19 -0600

    more changes

--- template.lua
+++ template.lua
@@ -1,1 +1,36 @@
-/uufs/chpc.utah.edu/sys/modulefiles/templates/template.lua
+-- -*- lua -*-
+
+-- Lua's comment line starts with "--"
+
+-- example version - needs to be at the top so that other sections have it
+local version = "18.1"
+
+help([[
+]])
+
+--At least put a name, version, and URL. 
+--some of the others are kind of optional
+whatis("Name         : ")
+whatis("Version      : " .. version)
+whatis("Category     : ")
+whatis("Description  : ")
+whatis("URL          : ")
+whatis("Installed on : ")
+whatis("Modified on  : ")
+whatis("Installed by : ")
+
+-- example base installation directory
+-- if the module name is the same as installation directory, we can also use
+-- the myModuleName() function, otherwise replace myModuleName() with explicit directory name
+local base = pathJoin("/uufs/chpc.utah.edu/sys/installdir", myModuleName(), version)
+
+-- often we want to define the base directory of the package
+setenv("MYPKG",base)
+-- and prepend the PATH
+prepend_path("PATH",pathJoin(base,"bin")
+
+-- for libraries, we want to set the INCDIR and LIBDIR, e.g.
+-- setenv("NETCDFC_INCDIR",pathJoin(base,"/include"))
+-- setenv("NETCDFC_LIBDIR",pathJoin(base,"/lib"))
+
+-- for other module files features see https://github.com/CHPC-UofU/Lmod-setup/blob/master/readme.md#moduleformat
--- update_cache.sh
+++ update_cache.sh
@@ -1,1 +1,30 @@
-/uufs/chpc.utah.edu/sys/modulefiles/scripts/caching/update_cache.sh
+#!/bin/bash
+
+#if [ `hostname` == "centos7.chpc.utah.edu" ]; then
+if [ -z "$LMOD_VERSION" ]; then
+  source /etc/profile.d/chpc.sh
+fi
+   
+env >& ~/cron.txt
+
+PATH_TO_LMOD_LIBEXEC=/uufs/chpc.utah.edu/sys/installdir/lmod/7.7.29/libexec/
+#LMOD_DEFAULT_MODULEPATH="/uufs/chpc.utah.edu/sys/modulefiles/CHPC-18/Core:/uufs/chpc.utah.edu/sys/installdir/lmod/7.7.29/modulefiles/Core"
+MYMODULEPATH="/uufs/chpc.utah.edu/sys/modulefiles/CHPC-18/Core"
+LMOD_CACHEDIR=/uufs/chpc.utah.edu/sys/installdir/lmod/cache/7.7-c7
+LMOD_TIMESTAMP_FN=/uufs/chpc.utah.edu/sys/installdir/lmod/systemfiles/7.7-c7/system.txt
+$PATH_TO_LMOD_LIBEXEC/update_lmod_system_cache_files -D -t $LMOD_TIMESTAMP_FN -d $LMOD_CACHEDIR $MYMODULEPATH |& tee $LMOD_CACHEDIR/cache.out
+
+#if [ $? -ne 0 ]; then
+if grep -q ERROR $LMOD_CACHEDIR/cache.out; then
+  echo "Cache generation error"
+  # copy the backup files
+  # the cache generation failed, send e-mail to operations? including content of $LMOD_CACHEDIR/cache.out
+  ( echo -e "Error generating Lmod cache.\n\n" ; cat $LMOD_CACHEDIR/cache.out ) | mail -s "Lmod cache error" david.richardson@utah.edu martin.cuma@utah.edu
+  exit 1
+#else
+#  rm $LMOD_CACHEDIR/cache.out
+fi
+
+# MC addition - update module reverse map for XALT
+# not needed - better to do this before uploading XALT data to the database
+#/uufs/chpc.utah.edu/sys/installdir/lmod/lmod/libexec/spider -o jsonReverseMapT  $LMOD_DEFAULT_MODULEPATH  > /uufs/chpc.utah.edu/sys/installdir/xalt/std/rmapD/jsonReverseMapT.json
